e7309b6c77e6e6bb6099436d6a6d6b17d1bc1d39
jpontel <jpontel2002@gmail.com>
2025-01-28 12:22:50 -0300

    form...

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
 import {GenericButtonComponent} from '../shared/generic-button/generic-button.component';
+import {AccountService} from '../_services/account.service';
+import {Component, inject} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
   imports: [
-    GenericButtonComponent
+    GenericButtonComponent,
+    FormsModule
   ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  private accountService = inject(AccountService);
+  loggedIn: boolean = false;
+  model: any = {};
 
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: (response) => {
+        this.loggedIn = true;
+        console.log('Login Successful', response);
+      },
+      error: (error) => {
+        console.log('Login Failed', error);
+      }
+    });
+  };
+
+  logout() {
+    this.loggedIn = false;
+  }
 }
